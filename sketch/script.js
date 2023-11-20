416281573eaf96c20625293cb16c5dbc3d283fbc
luka-danger <lukathevegan@gmail.com>
2023-11-20 09:29:23 -0700

    Add DOM Manipulation
    The code creates a div and a container, but it fills it green instead of creating a 16x16 grid of divs

--- sketch/script.js
+++ sketch/script.js
@@ -2,3 +2,14 @@
 window.addEventListener('load', (event) => {
     console.log('The page has fully loaded');
 }); 
+
+const grid = document.querySelector('.grid');
+
+// Use nested for loop to create 16x16 divs
+for(let i = 0; i < 16; i++) {
+    for(let j = 0; j < 16; j++) {
+        const gridDiv = document.createElement('div');
+        gridDiv.classList.add('div'); 
+        grid.appendChild(gridDiv); 
+    };
+}; 
