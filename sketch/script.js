029e299f04b54c4aa154118dfa388ea468efa3eb
luka-danger <lukathevegan@gmail.com>
2023-11-20 09:57:36 -0700

    Update function in for loop

--- sketch/script.js
+++ sketch/script.js
@@ -8,13 +8,14 @@ const grid = document.querySelector('.grid');
 // Use nested for loop to create 16x16 divs
 for(let i = 0; i < 16; i++) {
     for(let j = 0; j < 16; j++) {
-        const gridDiv = document.createElement('div');
+        const gridDiv = document.createElement('div')
         gridDiv.classList.add('div'); 
         grid.appendChild(gridDiv); 
-    };
-    addEventListener('mouseover', () => {
-        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-        grid.style.background = '#' + randomColor; 
-    }); 
+        // Change the color of the divs
+        addEventListener('mouseover', () => {
+            const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+            gridDiv.style.background = '#' + randomColor; 
+        }); 
+    }; 
 }; 
 
