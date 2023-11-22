09bca1a029eedf255ac4b510a1c2559d3a6e2de0
luka-danger <lukathevegan@gmail.com>
2023-11-21 22:00:18 -0700

    Move gridDiv variable outside for loop
    This creates one div, instead of a 16x16. Try different logic

--- sketch/script.js
+++ sketch/script.js
@@ -4,15 +4,16 @@ window.addEventListener('load', (event) => {
 }); 
 
 const grid = document.querySelector('.grid');
+let gridDiv = document.createElement('div')
 
 // Use nested for loop to create 16x16 divs
 for(let i = 0; i < 16; i++) {
     for(let j = 0; j < 16; j++) {
-        const gridDiv = document.createElement('div')
+        //let gridDiv = document.createElement('div')
         gridDiv.classList.add('div'); 
         grid.appendChild(gridDiv); 
         // Change the color of the divs
-        addEventListener('mouseover', () => {
+        addEventListener('click', () => {
             const randomColor = Math.floor(Math.random() * 16777215).toString(16);
             gridDiv.style.background = '#' + randomColor; 
         }); 
