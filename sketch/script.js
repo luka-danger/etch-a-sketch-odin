e12d6da1cb9c126d80e6957535ab202ebde12da9
luka-danger <lukathevegan@gmail.com>
2023-11-21 23:25:00 -0700

    Replace nested for loop with single for loop

--- sketch/script.js
+++ sketch/script.js
@@ -5,16 +5,14 @@ window.addEventListener('load', (event) => {
 
 const grid = document.querySelector('.grid');
 
-// Use nested for loop to create 16x16 divs
-for(let i = 0; i < 16; i++) {
-    for(let j = 0; j < 16; j++) {
-        const gridDiv = document.createElement('div')
-        gridDiv.classList.add('div'); 
-        grid.appendChild(gridDiv); 
-        // Change the color of the divs
-        addEventListener('mouseover', () => {
-            const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-            gridDiv.style.background = '#' + randomColor; 
-        }); 
-    }; 
+for(let i = 0; i < (16 * 16); i++) {
+    const gridDiv = document.createElement('div')
+    gridDiv.classList.add('div'); 
+    grid.appendChild(gridDiv); 
+    // Change the color of the divs
+    addEventListener('click', () => {
+        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+        gridDiv.style.background = '#' + randomColor; 
+    }); 
 }; 
+
