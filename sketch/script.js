2d7a283c999f59d67e71c55bd21f68da61442336
luka-danger <lukathevegan@gmail.com>
2023-11-22 20:54:03 -0700

    Add event listener
    The event listener changes the color of each individual div

--- sketch/script.js
+++ sketch/script.js
@@ -6,16 +6,17 @@ window.addEventListener('load', (event) => {
 function makeBoard(size) {
     let grid = document.querySelector('.grid');
     let boxes = document.querySelectorAll('div'); 
-    // Clear grid
-    boxes.forEach((div) => div.remove()); 
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     let boardSize = size * size
     for(let i = 0; i < boardSize; i++) {
-        const box = document.createElement('div')
+        const box = document.createElement('div');
         box.classList.add('div'); 
         grid.appendChild(box); 
+        box.addEventListener("mouseover", () => {
+            box.style.backgroundColor = "green";
+        }); 
     };
 }; 
-makeBoard(16)
+makeBoard(50)
