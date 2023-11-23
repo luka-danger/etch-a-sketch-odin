1493fa828478170f6a98958b66eaeb6d9519515f
luka-danger <lukathevegan@gmail.com>
2023-11-22 21:12:17 -0700

    Add button
    Enter amount of squares. Button is not yet functional

--- sketch/script.js
+++ sketch/script.js
@@ -5,18 +5,18 @@ window.addEventListener('load', (event) => {
 
 function makeBoard(size) {
     let grid = document.querySelector('.grid');
-    let boxes = document.querySelectorAll('div'); 
+    let squares = document.querySelectorAll('div'); 
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
     let boardSize = size * size
     for(let i = 0; i < boardSize; i++) {
-        const box = document.createElement('div');
-        box.classList.add('div'); 
-        grid.appendChild(box); 
+        const square = document.createElement('div');
+        square.classList.add('div'); 
+        grid.appendChild(square); 
         let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-        box.addEventListener("mouseover", () => {
-            box.style.backgroundColor = "#" + randomColor;
+        square.addEventListener("mouseover", () => {
+            square.style.backgroundColor = "#" + randomColor;
         }); 
     };
 }; 
