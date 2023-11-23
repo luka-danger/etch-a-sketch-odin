25e5a7197e5f0bf616dcf7ddd2d2d194a56022eb
luka-danger <lukathevegan@gmail.com>
2023-11-22 20:32:18 -0700

    Refactor code
    I am changing up my logic as I was stuck last time.

--- sketch/script.js
+++ sketch/script.js
@@ -3,16 +3,16 @@ window.addEventListener('load', (event) => {
     console.log('The page has fully loaded');
 }); 
 
-const grid = document.querySelector('.grid');
+function makeBoard(size) {
+    let grid = document.querySelector('.grid');
 
-for(let i = 0; i < (16 * 16); i++) {
-    const gridDiv = document.createElement('div')
-    gridDiv.classList.add('div'); 
-    grid.appendChild(gridDiv); 
-    // Change the color of the divs
-    addEventListener('click', () => {
-        const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-        gridDiv.style.background = '#' + randomColor; 
-    }); 
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for(let i = 0; i < (16 * 16); i++) {
+        const gridDiv = document.createElement('div')
+        gridDiv.classList.add('div'); 
+        grid.appendChild(gridDiv); 
+    };
 }; 
-
+makeBoard(16)
