8ae8aa5d21f6ca81ee68a0f23bc8859391393163
luka.danger <lukathevegan@gmail.com>
2023-11-24 12:45:36 -0700

    Update changes
    Change color buttons not working

--- sketch/script.js
+++ sketch/script.js
@@ -8,32 +8,51 @@ function makeBoard(size) {
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-
     let boardSize = size * size
     for (let i = 0; i < boardSize; i++) {
         const square = document.createElement('square');
         square.classList.add('square');
         grid.appendChild(square);
+        let randomColor = Math.floor(Math.random() * 16777215).toString(16);
         square.addEventListener("mouseover", () => {
-            square.style.backgroundColor = "black";
+            square.style.backgroundColor = "#" + randomColor;
         });
     };
 };
 
-start.addEventListener('click', function () {
-    makeBoard(16)
-    square.style.backgroundColor = "black";
+function randomColor() {
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "#" + randomColor;
+    });
+};
+
+random.addEventListener('click', function () {
+    randomColor();
+});
+
+function blackColor() {
+    square.addEventListener("mouseover", () => {
+        square.style.backgroundColor = "black";
+    });
+};
+
+black.addEventListener('click', function () {
+    blackColor();
 });
 
 size16.addEventListener('click', function () {
+    clearBoard()
     makeBoard(16)
 });
 
 size32.addEventListener('click', function () {
+    clearBoard()
     makeBoard(32)
 });
 
 size64.addEventListener('click', function () {
+    clearBoard()
     makeBoard(64)
 });
 
@@ -44,10 +63,4 @@ function clearBoard() {
 
 clear.addEventListener('click', function () {
     clearBoard()
-});
-
-
-let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-square.addEventListener("mouseover", () => {
-    square.style.backgroundColor = "#" + randomColor;
 });
